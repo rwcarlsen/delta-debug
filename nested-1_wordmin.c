27088328978853d8379034b3d16e71d1135a83e3
Robert Carlsen <rwcarlsen@gmail.com>
2012-11-06 15:47:19 -0600

    updated nested minimizations to reflect proper warning/error preservation changes

--- nested-1_wordmin.c
+++ nested-1_wordmin.c
@@ -1,17 +1,1 @@
-printf (const *, ...);
-*memset (void size_t);
-bar (int (*)(), int, void *);
-
-main(int argc, char **argv) { 
-  struct s { 
-    int b[argc];
-  };
-
-  int nested (struct s x) { 
-    sizeof(x);
-  }
-
-  struct s t;
-  printf("%d\n", bar (nested, argc, &t));
-}
-
+typedef __SIZE_TYPE__ size_t; int printf (const char *, ...); void *memset (void *, int, size_t); int bar (int (*)(), int, void *); main(int argc, char **argv) { struct s { int b[argc]; }; int nested (struct s x) { sizeof(x); } struct s t; printf("%d\n", bar (nested, argc, &t)); }
